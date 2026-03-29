b7be2e2df5972b616ab26c9a08b5399c0f117a87
benhaim254 <benowuor254@gmail.com>
2026-03-29 09:50:59 +0300

    Adding Database Records

--- backend/core/models.py
+++ backend/core/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
+    First_name = models.CharField(max_length=16)
+    Last_name = models.CharField(max_length=16)
+    email = models.EmailField(max_length=255)
+
+    def __str__(self):
+        return f"ID:{self.id}, Name: {self.First_name} {self.Last_name}, Email: {self.email}"
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,12 +1,5 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Welcome to the Hans API!")
-
-
-@login_required
-def secret(request):
-    return render(request, "core/secret.html")
+    return render(request, "core/index.html")
